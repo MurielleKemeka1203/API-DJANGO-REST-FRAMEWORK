346b149cb365143c1cad2b3edca35108f0c412d2
MurielleKemeka <kemeka716gmail.com>
2025-03-07 10:52:59 +0100

    install djangorestframework

---
+++ project/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+from shop.views import CategoryViewset, ProductViewset, ArticleViewset
+
+#Ici nous créons notre routeur
+router = routers.SimpleRouter()
+#Puis déclarons une url basée sur le mot clé 'category' et notre view
+#afin que l'url générée soit celle que nous souhaitons
+router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
+router.register('article', ArticleViewset, basename='article')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)), #il faut bien penser à ajouter les
+#urls du router dans la liste des urls disponibles
+]
